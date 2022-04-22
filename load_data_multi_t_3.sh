a7d231c10c3a4a7ba78ab27061a8d83b83dc4948
qingsuijiu <642782632@qq.com>
2022-04-22 15:10:00 +0800

    add tpc-ch load_data script

---
+++ load_data_multi_t_3.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=/usr/local/mysql/lib/mysql/
+mysql_host=$1
+mysql_user=$2
+mysql_password=$3
+mysql_port=$4
+db=$5
+scale=$6  #仓库数
+STEP=100
+
+./tpcc_load -h ${mysql_host} -P ${mysql_port} -d ${db} -u ${mysql_user} -p ${mysql_password} -w ${scale} -l 1 -m 1 -n ${scale} >> 1.out &
+
+x=1
+
+while [ $x -le ${scale} ]
+do
+         echo $x $(( $x + $STEP - 1 ))
+         ./tpcc_load -h ${mysql_host} -P ${mysql_port} -d ${db} -u ${mysql_user} -p ${mysql_password} -w ${scale} -l 2 -m $x -n $(( $x + $STEP - 1 ))  >> 2_$x.out &
+         ./tpcc_load -h ${mysql_host} -P ${mysql_port} -d ${db} -u ${mysql_user} -p ${mysql_password} -w ${scale} -l 3 -m $x -n $(( $x + $STEP - 1 ))  >> 3_$x.out &
+         ./tpcc_load -h ${mysql_host} -P ${mysql_port} -d ${db} -u ${mysql_user} -p ${mysql_password} -w ${scale} -l 4 -m $x -n $(( $x + $STEP - 1 ))  >> 4_$x.out &
+          x=$(( $x + $STEP ))
+done
+
+echo "load_data_multi begine"
